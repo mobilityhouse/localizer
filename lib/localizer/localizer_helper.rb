9161237d2b9f3b1666dc799f052645743acd4957
Bartosz Hejman <bartoszhejman@gmail.com>
2015-03-06 12:53:49 +0100

    EBELP-803 Refactor helper

--- lib/localizer/localizer_helper.rb
+++ lib/localizer/localizer_helper.rb
@@ -1,15 +1,20 @@
 module Localizer
   module LocalizerHelper
-    def sorted_languages_by_country(current_country, with_country = false)
-      locales_service.by_country(current_country).sort_by { |locale| locale.sort_key(with_country) }
+    def sorted_languages_by_country(oem, current_country, with_country = false)
+      Localizer::LocalesService.new(oem).by_country(current_country).sort_by { |locale| locale.sort_key(with_country) }
     end
 
-    def sorted_languages(with_country = false)
-      locales_service.locales.sort_by { |locale| locale.sort_key(with_country) }
+    def sorted_languages(oem, with_country = false)
+      Localizer::LocalesService.new(oem).locales.sort_by { |locale| locale.sort_key(with_country) }
     end
+
 
     def link_to_locale(locale, with_country = false)
       link_to locale.name_to_display(with_country), url_for(country: locale.country, language: locale.language)
+    end
+
+    def collection_for_locale_select(oem, with_country = false)
+      sorted_languages(oem, with_country).map { |locale| [locale.name_to_display(with_country), locale.to_s] }
     end
 
     def collection_for_country_select(oem)
